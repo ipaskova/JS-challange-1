3e888cdd956c401ab021ce90cc6e121b56e072c2
Ivona Paskova <45354556+ipaskova@users.noreply.github.com>
2019-02-20 07:59:00 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function messageFuel() {
+document.getElementById('paragraphFuel').innerHTML = 'Your fueltank is full!';
+}
+
+function messageSupplies(){
+document.getElementById('paragraphSupplies').innerHTML = 'Your supplies are sufficient!';
+	}
+
+	function messageSpeed(){
+		
+document.getElementById('paragraphSpeed').innerHTML = 'The average speed of the spaceship is 7.9 km/s.';
+	}
+
